e4242ea5eea5c8baae2573850359d177ea186e67
Nisarg08z <nisarg693z@gmail.com>
2026-01-09 10:23:04 +0530

    Day - 7.0

--- Server/src/index.js
+++ Server/src/index.js
@@ -4,9 +4,15 @@ import { Server } from "socket.io"
 import connectDB from "./db/index.js";
 import {app} from './app.js'
 import { setIO } from "./utils/socket.js";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 dotenv.config({
-    path: './.env'
+    path: join(__dirname, '../.env')
 })
 
 const httpServer = createServer(app);
